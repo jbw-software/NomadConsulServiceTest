26ecf2758541875ea700d844fd538c3dee8cd676
Jörg Wille <joerg.wille@gmail.com>
2023-01-10 20:22:04 +0100

    initial commit of sources for POC

---
+++ src/test/java/net/jbw/playground/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jbw.playground.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
